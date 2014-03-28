3e19f59b0e91db160c2e411e87f757bee0e08ae7
Spenser Shumaker <sshumake@redhat.com>
2014-03-28 13:46:53 -0400

    Code update
    Spenser Shumaker <sshumake@redhat.com>

--- app/log_viewer/log_viewer.module.js
+++ app/log_viewer/log_viewer.module.js
@@ -1,174 +1,232 @@
 // angular module
-var logViewer = angular.module('logViewer', ['angularTreeview',
-	'ui.bootstrap', 'RedhatAccess.search'
-]);
+var logViewer = angular.module('logViewer',
+		[ 'angularTreeview', 'ui.bootstrap', 'RedhatAccess.search']);
 
-logViewer.factory('files', function () {
+logViewer.factory('files', function() {
 	var fileList = '';
 	var selectedFile = '';
 	var selectedHost = '';
 	var file = '';
 	return {
-		getFileList: function () {
+		getFileList : function() {
 			return fileList;
 		},
 
-		setFileList: function (fileList) {
+		setFileList : function(fileList) {
 			this.fileList = fileList;
 		},
-		getSelectedFile: function () {
+		getSelectedFile : function() {
 			return selectedFile;
 		},
 
-		setSelectedFile: function (selectedFile) {
+		setSelectedFile : function(selectedFile) {
 			this.selectedFile = selectedFile;
 		},
-		getFile: function () {
+		getFile : function() {
 			return file;
 		},
 
-		setFile: function (file) {
+		setFile : function(file) {
 			this.file = file;
 		}
 	};
 });
 
-logViewer.service('accordian', function () {
+logViewer.service('accordian', function() {
 	var groups = new Array();
 	return {
-		getGroups: function () {
+		getGroups : function() {
 			return groups;
 		},
-		addGroup: function (group) {
+		addGroup : function(group) {
 			groups.push(group);
 		},
-		clearGroups: function () {
+		clearGroups : function() {
 			groups = '';
 		}
 	};
 });
 
-
-angular.element(document).ready(function () {
+angular.element(document).ready(function() {
 
 	c = angular.element(document.querySelector('#controller-demo')).scope();
 });
 
-logViewer.controller('fileController', function ($scope, files) {
+logViewer.controller('fileController', function($scope, files) {
 	$scope.roleList = '';
-	$scope.updateSelected = function () {
-		if ($scope.mytree.currentNode != null) {
-			//files.setSelectedFile('/Users/Spense/Desktop/server.log');
-			files.setSelectedFile($scope.mytree.currentNode.roleName);
+	$scope.updateSelected = function() {
+		if ($scope.mytree.currentNode != null
+				&& $scope.mytree.currentNode.fullPath != null) {
+			files.setSelectedFile($scope.mytree.currentNode.fullPath);
+			// files.setSelectedFile($scope.mytree.currentNode.roleName);
 		}
 	};
-	$scope.$watch(function () {
+	$scope.$watch(function() {
 		return files.fileList;
-	}, function () {
+	}, function() {
 		$scope.roleList = files.fileList;
 	});
 });
 
-logViewer.controller('DropdownCtrl', function ($scope, $http, files) {
+logViewer.controller('DropdownCtrl', function($scope, $http, files) {
 	$scope.blah = "Please Select the Machine";
 	$scope.items = [];
-	$scope.init = function () {
+	$scope.init = function() {
 		$http({
-			method: 'GET',
-			url: 'log_viewer/GetMachineList'
-		}).success(function (data, status, headers, config) {
+			method : 'GET',
+			url : 'GetMachineList'
+		}).success(function(data, status, headers, config) {
 			$scope.items = data;
-		}).error(function (data, status, headers, config) {
+		}).error(function(data, status, headers, config) {
 			var i = 0;
 			// called asynchronously if an error occurs
 			// or server returns response with an error status.
 		});
 	};
-	$scope.machineSelected = function () {
+	$scope.machineSelected = function() {
 		files.selectedHost = this.choice;
 		$scope.blah = this.choice;
 		$http({
-			method: 'GET',
-			url: 'log_viewer/GetFileList'
-		}).success(function (data, status, headers, config) {
-			files.setFileList(data);
-		}).error(function (data, status, headers, config) {
+			method : 'GET',
+			url : 'GetFileList'
+		}).success(function(data, status, headers, config) {
+			var tree = new Array();
+			parseList(tree, data);
+			console.log(tree)
+			var string = JSON.stringify(tree);
+			files.setFileList(tree);
+		}).error(function(data, status, headers, config) {
 			// called asynchronously if an error occurs
 			// or server returns response with an error status.
 		});
 	};
 });
 
-logViewer.controller('selectFileButton', function ($scope, $http, files) {
-	$scope.fileSelected = function () {
-		$http({
-			method: 'GET',
-			url: 'log_viewer/GetLogFile?filePath=' + files.selectedFile + '&hostName=localhost'
-		}).success(function (data, status, headers, config) {
+logViewer.controller('selectFileButton', function($scope, $http, files) {
+	$scope.fileSelected = function() {
+		$scope.$parent.opens = !$scope.$parent.opens;
+		$http(
+				{
+					method : 'GET',
+					url : 'GetLogFile?filePath=' + files.selectedFile
+							+ '&hostName=localhost'
+				}).success(function(data, status, headers, config) {
 			files.file = data;
-		}).error(function (data, status, headers, config) {
+		}).error(function(data, status, headers, config) {
 			// called asynchronously if an error occurs
 			// or server returns response with an error status.
 		});
 	};
 });
 
-logViewer.controller('TabsDemoCtrl', ['$scope', 'files', 'accordian', 'SearchResultsService',
-	function ($scope, files, accordian, SearchResultsService) {
-		$scope.tabs = [{
-			shortTitle: "Short Sample Log File",
-			longTitle: "Long Log File",
-			content: "Sample Log Text"
-		}];
+logViewer.controller('TabsDemoCtrl', [
+		'$scope',
+		'files',
+		'accordian',
+		'SearchResultsService',
+		function($scope, files, accordian, SearchResultsService) {
+			$scope.tabs = [ {
+				shortTitle : "Short Sample Log File",
+				longTitle : "Long Log File",
+				content : "Sample Log Text"
+			} ];
 
-		$scope.$watch(function () {
-			return files.file;
-		}, function () {
-			if (files.file != null && files.selectedFile != null) {
-				file = new Object();
-				file.longTitle = files.selectedHost + " : " + files.selectedFile;
-				var splitFileName = files.selectedFile.split("/");
-				var fileName = splitFileName[splitFileName.length - 1];
-				file.shortTitle = files.selectedHost + ":" + fileName;
-				file.content = files.file;
-				files.file = null;
-				$scope.tabs.push(file);
-			}
-		});
-		$scope.removeTab = function (index) {
-			$scope.tabs.splice(index, 1);
-		};
+			$scope.$watch(function() {
+				return files.file;
+			}, function() {
+				if (files.file != null && files.selectedFile != null) {
+					file = new Object();
+					file.longTitle = files.selectedHost + " : "
+							+ files.selectedFile;
+					var splitFileName = files.selectedFile.split("/");
+					var fileName = splitFileName[splitFileName.length - 1];
+					file.shortTitle = files.selectedHost + ":" + fileName;
+					file.content = files.file;
+					files.file = null;
+					$scope.tabs.push(file);
+				}
+			});
+			$scope.removeTab = function(index) {
+				$scope.tabs.splice(index, 1);
+			};
 
-		$scope.checked = false; // This will be binded using the ps-open attribute
+			$scope.checked = false; // This will be
+			// binded using the
+			// ps-open attribute
 
-		$scope.diagnoseText = function () {
+			$scope.diagnoseText = function() {
+				if (!$scope.$parent.open) {
+					$scope.$parent.open = !$scope.$parent.open;
+				}
+				var text = "";
+				if (window.getSelection) {
+					text = window.getSelection().toString();
+				} else if (document.selection
+						&& document.selection.type != "Control") {
+					text = document.selection.createRange().text;
+				}
+				if (text != "") {
+					$scope.checked = !$scope.checked;
+					SearchResultsService.diagnose(text, 5);
+					// strata.diagnose(text, onSuccess = function(response) {
+					// var group = new Object();
+					// group.title = response.title;
+					// group.content = response.issue.text;
+					// accordian.addGroup(group);
+					// $scope.$apply();
+					// }, onFailure = function(response) {
+					// // Iterate over the response array
+					// // response.forEach(someHandler);
+					// console.log(response);
+					// }, 5);
+				}
+			};
+		} ]);
 
-			var text = "";
-			if (window.getSelection) {
-				text = window.getSelection().toString();
-			} else if (document.selection && document.selection.type != "Control") {
-				text = document.selection.createRange().text;
-			}
-			if (text != "") {
-				$scope.checked = !$scope.checked;
-				SearchResultsService.diagnose(text, 5);
-				// strata.diagnose(text, onSuccess = function (response) {
-				// 	var group = new Object();
-				// 	group.title = response.title;
-				// 	group.content = response.issue.text;
-				// 	accordian.addGroup(group);
-				// 	$scope.$apply();
-				// }, onFailure = function (response) {
-				// 	// Iterate over the response array
-				// 	// response.forEach(someHandler);
-				// 	console.log(response);
-				// }, 5);
-			}
-		};
-	}
-]);
-
-logViewer.controller('AccordionDemoCtrl', function ($scope, accordian) {
+logViewer.controller('AccordionDemoCtrl', function($scope, accordian) {
 	$scope.oneAtATime = true;
 	$scope.groups = accordian.getGroups();
 });
+
+function parseList(tree, data) {
+	var files = data.split("\n");
+	for ( var i in files) {
+		var file = files[i];
+		var splitPath = file.split("/");
+		returnNode(splitPath, tree, file);
+	}
+}
+
+function returnNode(splitPath, tree, fullFilePath) {
+	if (splitPath[0] != null) {
+		if (splitPath[0] != "") {
+			var node = splitPath[0];
+			var match = false;
+			var index = 0;
+			for ( var i in tree) {
+				if (tree[i].roleName == node) {
+					match = true;
+					index = i;
+					break;
+				}
+			}
+			if (!match) {
+				var blah = new Object();
+				blah.roleName = node;
+				blah.roleId = node;
+				if (splitPath.length == 1) {
+					blah.fullPath = fullFilePath;
+				}
+				blah.children = new Array();
+				tree.push(blah);
+				index = tree.length - 1;
+			}
+
+			splitPath.shift();
+			returnNode(splitPath, tree[index].children, fullFilePath);
+		} else {
+			splitPath.shift();
+			returnNode(splitPath, tree, fullFilePath);
+		}
+	}
+}
