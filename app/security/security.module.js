c26c6cb20b1b24b8d6cebcdb424efc622f000792
Dan Varga <dvarga@redhat.com>
2014-04-15 19:51:37 -0400

    Add ui.router to security module

--- app/security/security.module.js
+++ app/security/security.module.js
@@ -1,4 +1,4 @@
-angular.module('RedhatAccess.security', ['ui.bootstrap', 'templates.app'])
+angular.module('RedhatAccess.security', ['ui.bootstrap', 'templates.app', 'ui.router'])
     .constant('AUTH_EVENTS', {
         loginSuccess: 'auth-login-success',
         loginFailed: 'auth-login-failed',
