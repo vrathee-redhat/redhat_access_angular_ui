1ba7c7c08ccfe9a63aa04d5fc60ddee584da0770
ckyrouac <ckyrouac@redhat.com>
2014-04-24 12:46:56 -0400

    Make alerts closeable

--- app/common/header.module.js
+++ app/common/header.module.js
@@ -20,6 +20,11 @@ angular.module('RedhatAccess.header', [])
 .controller('AlertController', ['$scope', 'AlertService',
     function ($scope, AlertService) {
       $scope.AlertService = AlertService;
+      $scope.closeable = true;
+
+      $scope.closeAlert = function(index) {
+        AlertService.alerts.splice(index, 1);
+      }
     }])
 .directive('rhaAlert',
     function () {
