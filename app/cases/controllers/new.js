0217339585164fc486fca1eb6002d1062663cd9c
lphiri <lphiri@redhat.com>
2014-04-24 14:04:42 -0400

    Fix beautifier formatting

--- app/cases/controllers/new.js
+++ app/cases/controllers/new.js
@@ -15,7 +15,7 @@ angular.module('RedhatAccess.cases')
     '$rootScope',
     'AUTH_EVENTS',
     '$location',
-    function($scope,
+    function ($scope,
       $state,
       $q,
       SearchResultsService,
@@ -44,20 +44,20 @@ angular.module('RedhatAccess.cases')
       $scope.RecommendationsService = RecommendationsService;
       $scope.securityService = securityService;
 
-      $scope.getRecommendations = function() {
+      $scope.getRecommendations = function () {
         SearchResultsService.searchInProgress.value = true;
         RecommendationsService.populateRecommendations(5).then(
-          function() {
+          function () {
             SearchResultsService.clear();
 
             RecommendationsService.recommendations.forEach(
-              function(recommendation) {
+              function (recommendation) {
                 SearchResultsService.add(recommendation);
               }
             )
             SearchResultsService.searchInProgress.value = false;
           },
-          function(error) {
+          function (error) {
             AlertService.addStrataErrorMessage(error);
           }
         );
@@ -66,56 +66,56 @@ angular.module('RedhatAccess.cases')
       /**
        * Populate the selects
        */
-      $scope.initSelects = function() {
+      $scope.initSelects = function () {
         $scope.productsLoading = true;
         strataService.products.list().then(
-          function(products) {
+          function (products) {
             $scope.products = products;
             $scope.productsLoading = false;
           },
-          function(error) {
+          function (error) {
             AlertService.addStrataErrorMessage(error);
           }
         );
 
         $scope.severitiesLoading = true;
         strataService.values.cases.severity().then(
-          function(severities) {
+          function (severities) {
             $scope.severities = severities;
             CaseService.
-            case .severity = severities[severities.length - 1];
-            $scope.severitiesLoading = false;
+          case .severity = severities[severities.length - 1];
+          $scope.severitiesLoading = false;
           },
-          function(error) {
+          function (error) {
             AlertService.addStrataErrorMessage(error);
           }
         );
 
         $scope.groupsLoading = true;
         strataService.groups.list().then(
-          function(groups) {
+          function (groups) {
             $scope.groups = groups;
             $scope.groupsLoading = false;
           },
-          function(error) {
+          function (error) {
             AlertService.addStrataErrorMessage(error);
           }
         );
       };
 
-      $scope.initDescription = function() {
+      $scope.initDescription = function () {
         var searchObject = $location.search();
         if (searchObject.data) {
           CaseService.case.description = searchObject.data;
-        } else { 
-           //angular does not  handle params before hashbang
-           //@see https://github.com/angular/angular.js/issues/6172
+        } else {
+          //angular does not  handle params before hashbang
+          //@see https://github.com/angular/angular.js/issues/6172
           var queryParamsStr = window.location.search.substring(1);
           var parameters = queryParamsStr.split('&');
           for (var i = 0; i < parameters.length; i++) {
             var parameterName = parameters[i].split('=');
             if (parameterName[0] == 'data') {
-               CaseService.case.description = decodeURIComponent(parameterName[1]);
+              CaseService.case.description = decodeURIComponent(parameterName[1]);
             }
           }
         }
@@ -124,7 +124,7 @@ angular.module('RedhatAccess.cases')
       $scope.initSelects();
       $scope.initDescription();
 
-      $rootScope.$on(AUTH_EVENTS.loginSuccess, function() {
+      $rootScope.$on(AUTH_EVENTS.loginSuccess, function () {
         $scope.initSelects();
         $scope.initDescription();
         AlertService.clearAlerts();
@@ -133,19 +133,15 @@ angular.module('RedhatAccess.cases')
       /**
        * Set $scope.incomplete to boolean based on state of form
        */
-      $scope.validateForm = function() {
-        if (CaseService.
-          case .product == null || CaseService.
-          case .product == "" ||
-          CaseService.
-          case .version == null || CaseService.
-          case .version == "" ||
-          CaseService.
-          case .summary == null || CaseService.
-          case .summary == "" ||
-          CaseService.
-          case .description == null || CaseService.
-          case .description == "") {
+      $scope.validateForm = function () {
+        if (CaseService.case.product == null ||
+          CaseService.case.product == "" ||
+          CaseService.case.version == null ||
+          CaseService.case.version == "" ||
+          CaseService.case.summary == null ||
+          CaseService.case.summary == "" ||
+          CaseService.case.description == null ||
+          CaseService.case.description == "") {
           $scope.incomplete = true;
         } else {
           $scope.incomplete = false;
@@ -157,24 +153,24 @@ angular.module('RedhatAccess.cases')
        *
        * @param product
        */
-      $scope.getProductVersions = function(product) {
+      $scope.getProductVersions = function (product) {
         CaseService.
-        case .version = "";
-        $scope.versionDisabled = true;
-        $scope.versionLoading = true;
+      case .version = "";
+      $scope.versionDisabled = true;
+      $scope.versionLoading = true;
 
-        strata.products.versions(
-          product.code,
-          function(response) {
-            $scope.versions = response;
-            $scope.validateForm();
-            $scope.versionDisabled = false;
-            $scope.versionLoading = false;
-            $scope.$apply();
-          },
-          function(error) {
-            AlertService.addStrataErrorMessage(error);
-          });
+      strata.products.versions(
+        product.code,
+        function (response) {
+          $scope.versions = response;
+          $scope.validateForm();
+          $scope.versionDisabled = false;
+          $scope.versionLoading = false;
+          $scope.$apply();
+        },
+        function (error) {
+          AlertService.addStrataErrorMessage(error);
+        });
       };
 
       /**
@@ -182,7 +178,7 @@ angular.module('RedhatAccess.cases')
        *
        * @param page
        */
-      $scope.gotoPage = function(page) {
+      $scope.gotoPage = function (page) {
         $scope.isPage1 = page == 1 ? true : false;
         $scope.isPage2 = page == 2 ? true : false;
       };
@@ -190,14 +186,14 @@ angular.module('RedhatAccess.cases')
       /**
        * Navigate forward in the wizard
        */
-      $scope.doNext = function() {
+      $scope.doNext = function () {
         $scope.gotoPage(2);
       };
 
       /**
        * Navigate back in the wizard
        */
-      $scope.doPrevious = function() {
+      $scope.doPrevious = function () {
         $scope.gotoPage(1);
       };
 
@@ -205,47 +201,34 @@ angular.module('RedhatAccess.cases')
       /**
        * Create the case with attachments
        */
-      $scope.doSubmit = function() {
+      $scope.doSubmit = function () {
         var caseJSON = {
-          'product': CaseService.
-          case .product.code,
-          'version':
-            CaseService.
-          case .version,
-          'summary':
-            CaseService.
-          case .summary,
-          'description':
-            CaseService.
-          case .description,
-          'severity':
-            CaseService.
-          case .severity.name,
-          'folderNumber':
-            CaseService.
-          case .caseGroup == null ? '':
-            CaseService.
-          case .caseGroup.number
+          'product': CaseService.case.product.code,
+          'version':CaseService.case.version,
+          'summary':CaseService.case.summary,
+          'description':CaseService.case.description,
+          'severity':CaseService.case.severity.name,
+          'folderNumber':CaseService.case.caseGroup == null ? '' : CaseService.case.caseGroup.number
         };
         $scope.submittingCase = true;
         strata.cases.post(
           caseJSON,
-          function(caseNumber) {
+          function (caseNumber) {
             if ((AttachmentsService.updatedAttachments.length > 0) || (AttachmentsService.hasBackEndSelections())) {
               AttachmentsService.updateAttachments(caseNumber).then(
-                function() {
+                function () {
                   $state.go('edit', {
                     id: caseNumber
                   });
                   $scope.submittingCase = false;
                 },
-                function(error) {
+                function (error) {
                   AlertService.addStrataErrorMessage(error);
                 }
               );
             }
           },
-          function(error) {
+          function (error) {
             AlertService.addStrataErrorMessage(error);
           }
         );
