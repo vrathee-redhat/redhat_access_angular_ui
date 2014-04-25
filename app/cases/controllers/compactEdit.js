7d2cf452d50771ca86065c443b0052d5bb74c8ff
ckyrouac <ckyrouac@redhat.com>
2014-04-25 10:15:14 -0400

    Don't make strata call to get product versions when case has no product (compact)

--- app/cases/controllers/compactEdit.js
+++ app/cases/controllers/compactEdit.js
@@ -33,15 +33,16 @@ angular.module('RedhatAccess.cases')
             CaseService.defineCase(caseJSON);
             $scope.caseLoading = false;
 
-            strataService.products.versions(caseJSON.product.name).then(
-                function(versions) {
-                  CaseService.versions = versions;
-                },
-                function(error) {
-                  AlertService.addStrataErrorMessage(error);
-                }
-            );
-
+            if (caseJSON.product != null && caseJSON.product.name != null) {
+              strataService.products.versions(caseJSON.product.name).then(
+                  function(versions) {
+                    CaseService.versions = versions;
+                  },
+                  function(error) {
+                    AlertService.addStrataErrorMessage(error);
+                  }
+              );
+            }
             $scope.domReady = true;
           }
       );
