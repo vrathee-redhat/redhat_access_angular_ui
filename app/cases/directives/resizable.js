9fe599fe080e31cdaa0a5c4715436de9608ca894
lphiri <lphiri@redhat.com>
2014-05-14 09:29:38 -0400

    Fix scrolling bug

--- app/cases/directives/resizable.js
+++ app/cases/directives/resizable.js
@@ -1,46 +1,53 @@
 'use strict';
 
 angular.module('RedhatAccess.cases')
-.directive('rhaResizable', [
-  '$window',
-  '$timeout',
-  function ($window) {
+  .directive('rhaResizable', [
+    '$window',
+    '$timeout',
+    function ($window) {
 
-    var link = function(scope, element, attrs) {
+      var link = function (scope, element, attrs) {
 
-      scope.onResizeFunction = function() {
-        var distanceToTop = element[0].getBoundingClientRect().top;
-        var height = $window.innerHeight - distanceToTop;
-        element.css('height', height);
-      };
+        scope.onResizeFunction = function () {
+          var distanceToTop = element[0].getBoundingClientRect().top;
+          var height = $window.innerHeight - distanceToTop;
+          element.css('height', height);
+        };
 
-      angular.element($window).bind(
+        angular.element($window).bind(
           'resize',
-          function() {
+          function () {
             scope.onResizeFunction();
             scope.$apply();
           }
-      );
-
-      if (attrs.rhaDomReady !== undefined) {
-        scope.$watch('rhaDomReady', function(newValue) {
-          if (newValue) {
+        );
+        angular.element($window).bind(
+          'click',
+          function () {
             scope.onResizeFunction();
+            scope.$apply();
           }
-        });
-      } else {
-        scope.onResizeFunction();
-      }
+        );
+
+        if (attrs.rhaDomReady !== undefined) {
+          scope.$watch('rhaDomReady', function (newValue) {
+            if (newValue) {
+              scope.onResizeFunction();
+            }
+          });
+        } else {
+          scope.onResizeFunction();
+        }
 
 
-    };
+      };
 
-    return {
-      restrict: 'A',
-      scope: {
-        rhaDomReady: '='
-      },
-      link: link
-    };
-  }
-]);
+      return {
+        restrict: 'A',
+        scope: {
+          rhaDomReady: '='
+        },
+        link: link
+      };
+    }
+  ]);
