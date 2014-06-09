feef1c30c4dcb8a4b8c7516cb01601a702312c6f
lphiri <lphiri@redhat.com>
2014-06-09 12:30:11 -0400

    Removed temporary translation file

--- app/i18n/translations.js
+++ app/i18n/translations.js
@@ -1,5 +1,4 @@
 angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
 /* jshint -W100 */
-    gettextCatalog.setStrings('fr', {"Cancel":"Siyani","Deleting attachment:":"Kufufuta chomanga","Note:":"Chidziwitso","Password":"Chiphaso","Red Hat Login":"Cholowera","Successfully deleted attachment:":"Tamaliza kufufuta","Successfully uploaded attachment":"Tamaliza kumangilira chomanga","Unauthorized.":"Siinu ololedwa","to case":"ku pempho lachithandizo"});
 /* jshint +W100 */
 }]);
