5b7491f15271160d2c6ad4bd60d14a9489c95b10
Aritro Dasgupta <ardasgup@redhat.com>
2014-07-09 16:00:52 +0530

    Added test for update case controller - incomplete

--- test/spec/cases/cases.controllers.test.js
+++ test/spec/cases/cases.controllers.test.js
@@ -2,5 +2,33 @@
 
 describe('Case Controllers', function() {
 
+	var caseService;
+	var strataService;
+	var mockScope;
+
+	beforeEach(angular.mock.module('RedhatAccess.cases'));
+	beforeEach(function () {
+		inject(function ($injector, $rootScope) {
+			caseService = $injector.get('CaseService');
+			strataService = $injector.get('strataService');
+			mockScope = $rootScope.$new();
+		})
+	});
+
+	it('should have a function for updating case details', inject(function ($controller) {
+
+		$controller('DetailsSection', {
+			$scope: mockScope,
+			CaseService: caseService,
+			strataService: strataService
+		});
+
+		var caseJSON = {};
+		expect(mockScope.updateCase).toBeDefined();
+		//spyOn(strataService, 'method()');
+		//mockScope.updateCase("1234", caseJSON);
+		//expect(strataService.cases.put).toHaveBeenCalledWith("1234", caseJSON);
+
+	}));
 
 });
