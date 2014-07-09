36ca752dc379694af1d91eededae23f90fd4a125
Aritro Dasgupta <ardasgup@redhat.com>
2014-07-09 12:08:06 +0530

    Added test for linked bugzilla to case - directive

--- test/spec/cases/cases.directives.test.js
+++ test/spec/cases/cases.directives.test.js
@@ -2,5 +2,31 @@
 
 describe('Case Directives', function() {
 
+	var mockScope;
+    var compileService;
+    var securityService;
+
+
+    beforeEach(angular.mock.module('RedhatAccess.cases'));
+    beforeEach(angular.mock.inject(function($rootScope, $compile) {
+        mockScope = $rootScope.$new();
+        compileService = $compile;
+    }));
+
+    beforeEach(function() {
+        inject(function($injector) {
+            securityService = $injector.get('securityService');
+        })
+    });
+
+    it('should display the linked bugzilla section for internal user', function() {
+        var compileFn = compileService(' <rha-list-bugzillas/>');
+        var element = compileFn(mockScope);
+        securityService.loginStatus.isInternal = true;
+        mockScope.securityService = securityService;
+        mockScope.$digest();
+        expect(element.find('.redhat-access-bz').length).toBe(1);        
+    });
+
 
 });
