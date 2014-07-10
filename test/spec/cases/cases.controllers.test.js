2434ddd22badc87ba4543cb6a13b03d7a6435035
Aritro Dasgupta <ardasgup@redhat.com>
2014-07-10 10:53:55 +0530

    Added test for update case - DetailsSection controller

--- test/spec/cases/cases.controllers.test.js
+++ test/spec/cases/cases.controllers.test.js
@@ -2,33 +2,38 @@
 
 describe('Case Controllers', function() {
 
-	var caseService;
-	var strataService;
-	var mockScope;
+		var caseService;
+		var strataService;
+		var mockScope;
+		var q;
 
-	beforeEach(angular.mock.module('RedhatAccess.cases'));
-	beforeEach(function () {
-		inject(function ($injector, $rootScope) {
-			caseService = $injector.get('CaseService');
-			strataService = $injector.get('strataService');
-			mockScope = $rootScope.$new();
-		})
-	});
-
-	it('should have a function for updating case details', inject(function ($controller) {
-
-		$controller('DetailsSection', {
-			$scope: mockScope,
-			CaseService: caseService,
-			strataService: strataService
+		beforeEach(angular.mock.module('RedhatAccess.cases'));
+		beforeEach(function () {
+				inject(function ($injector, $rootScope, $q) {
+						caseService = $injector.get('CaseService');
+						q = $q;
+						strataService = $injector.get('strataService');
+						mockScope = $rootScope.$new();					
+				})
 		});
 
-		var caseJSON = {};
-		expect(mockScope.updateCase).toBeDefined();
-		//spyOn(strataService, 'method()');
-		//mockScope.updateCase("1234", caseJSON);
-		//expect(strataService.cases.put).toHaveBeenCalledWith("1234", caseJSON);
+		it('should have a function for updating case details', inject(function ($controller) {
 
-	}));
+				$controller('DetailsSection', {
+						$scope: mockScope,
+						CaseService: caseService,
+						strataService: strataService
+				});
 
+				var caseJSON = {};
+				caseService.case.case_number = '1234';
+				expect(mockScope.updateCase).toBeDefined();
+				var deferred = q.defer();
+				spyOn(strataService.cases, 'put').andReturn(deferred.promise);
+		        deferred.resolve();
+		        mockScope.updateCase();
+		        expect(strataService.cases.put).toHaveBeenCalledWith("1234", caseJSON);
+				
+		}));
+	
 });
