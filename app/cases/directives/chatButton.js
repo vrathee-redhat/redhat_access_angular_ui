4e05b60f707d85019c94f89b78c0c9320731d438
lphiri <lphiri@redhat.com>
2014-07-25 20:33:31 -0400

    Isolate chat button directive scope

--- app/cases/directives/chatButton.js
+++ app/cases/directives/chatButton.js
@@ -2,10 +2,11 @@
 /*jshint unused:vars */
 
 angular.module('RedhatAccess.cases')
-.directive('rhaChatbutton', function () {
-  return {
-    templateUrl: 'cases/views/chatButton.html',
-    restrict: 'A',
-    controller: 'ChatButton'
-  };
-});
+	.directive('rhaChatbutton', function () {
+		return {
+			scope: {},
+			templateUrl: 'cases/views/chatButton.html',
+			restrict: 'A',
+			controller: 'ChatButton'
+		};
+	});
