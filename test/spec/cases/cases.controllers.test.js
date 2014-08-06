3f752eaf44b9416837618a7022ce235db18bc1db
Aritro Dasgupta <ardasgup@redhat.com>
2014-08-06 19:16:39 +0530

    Unit tests for case directives and controllers

--- test/spec/cases/cases.controllers.test.js
+++ test/spec/cases/cases.controllers.test.js
@@ -2,112 +2,344 @@
 
 describe('Case Controllers', function() {
 
-		var caseService;
-		var strataService;
-		var recommendationsService;
-		var mockScope;
-		var q;
+	var mockRecommendationsService;
+	var mockSearchResultsService;
+	var mockStrataService;
+    var mockStrataDataService;
+    var mockCaseService;
+	var mockScope;
+	var q;
 
-		beforeEach(angular.mock.module('RedhatAccess.cases'));
-		beforeEach(function () {
-				inject(function ($injector, $rootScope, $q) {
-						caseService = $injector.get('CaseService');
-						q = $q;
-						strataService = $injector.get('strataService');
-						recommendationsService = $injector.get('RecommendationsService');
-						mockScope = $rootScope.$new();					
-				})
-		});
+	beforeEach(angular.mock.module('RedhatAccess.cases'));
+	beforeEach(angular.mock.module('RedhatAccess.mock'));
 
-		it('should have a function for updating case details', inject(function ($controller) {
+	beforeEach(inject(function ($injector, $rootScope, $q) {
+		q = $q;
+		mockStrataService = $injector.get('strataService');
+		mockCaseService = $injector.get('MockCaseService');
+		mockRecommendationsService = $injector.get('MockRecommendationsService');
+		mockSearchResultsService = $injector.get('MockSearchResultsService');
+		mockStrataDataService = $injector.get('MockStrataDataService');
+		mockScope = $rootScope.$new();					
+			
+	}));
 
-				$controller('DetailsSection', {
-						$scope: mockScope,
-						CaseService: caseService,
-						strataService: strataService
-				});
+	//Suite for DetailsSection
+	describe('DetailsSection', function() {
 
-				var caseJSON = {};
-				caseService.kase.case_number = '1234';
-				expect(mockScope.updateCase).toBeDefined();
-				var deferred = q.defer();
-				spyOn(strataService.cases, 'put').andReturn(deferred.promise);
-		        deferred.resolve();
-		        mockScope.updateCase();
-		        expect(strataService.cases.put).toHaveBeenCalledWith("1234", caseJSON);
-				
+		it('should have a function for updating case details resolved', inject(function ($controller) {
+
+	        $controller('DetailsSection', {
+	            $scope: mockScope,
+	            CaseService: mockCaseService,
+	            strataService: mockStrataService
+	        });
+
+	        mockScope.caseDetails = {
+	          $valid: true,
+	          $setPristine: function() {}
+	        };
+
+	        mockCaseService.kase.case_number = '1234';
+	        mockCaseService.kase.type = 'bug';
+	        mockCaseService.kase.severity = 'high';
+	        mockCaseService.kase.status = 'open';
+	        mockCaseService.kase.alternate_id = '12345';
+	        mockCaseService.kase.product = 'Red Hat Enterprise Linux';
+	        mockCaseService.kase.version = '6.0';
+	        mockCaseService.kase.summary = 'Test Summary';
+	        mockCaseService.kase.group = {
+	          name: 'Test Group',
+	          number: '123456'
+	        };
+	        mockCaseService.kase.fts = true;
+	        mockCaseService.kase.contact_info24_x7 = 'test@test.com';
+	        expect(mockScope.updateCase).toBeDefined();
+	        mockScope.updateCase();
+	        spyOn(mockStrataService.cases, 'put').andCallThrough();
+	        mockScope.$root.$digest();
+	        expect(mockScope.updatingDetails).toBe(false);
+        
+	  	}));
+
+		it('should have a function to get Product Versions resolved', inject(function ($controller) {
+
+	        $controller('DetailsSection', {
+	            $scope: mockScope,
+	            CaseService: mockCaseService,
+	            strataService: mockStrataService
+	        });
+	        
+	        mockCaseService.kase.product = {
+	          name: 'Red Hat Enterprise Linux',
+	          code: '123456'
+	        };
+	       
+	        expect(mockScope.getProductVersions).toBeDefined();
+	        mockScope.getProductVersions();
+	        spyOn(mockStrataService.products, 'versions').andCallThrough();
+	        mockScope.$root.$digest();
+	        expect(mockCaseService.versions).toEqual(mockStrataDataService.mockVersions);   
+	        
+	  	}));
+
+	  	it('should have a function to get Product Versions rejected', inject(function ($controller) {
+
+	        $controller('DetailsSection', {
+	            $scope: mockScope,
+	            CaseService: mockCaseService,
+	            strataService: mockStrataService
+	        });
+
+	        mockCaseService.kase.product = {
+	          name: 'Red Hat Enterprise Linux',
+	          code: '123456'
+	        };
+	       
+	        expect(mockScope.getProductVersions).toBeDefined();
+	        mockStrataService.rejectCalls();
+	        spyOn(mockStrataService.products, 'versions').andCallThrough();
+	        mockScope.getProductVersions();        
+	        mockScope.$root.$digest();
+	        expect(mockCaseService.versions).toEqual([]);   
+	        
+	  	}));
+	});
+
+	//Suite for AddCommentSection
+	describe('AddCommentSection', function() {
+
+		it('should have a function for adding comments to case resolved', inject(function ($controller) {
+
+	        $controller('AddCommentSection', {
+	          	$scope: mockScope,
+	          	CaseService: mockCaseService,
+	          	strataService: mockStrataService
+	        });
+
+	      	mockCaseService.kase.case_number = '1234';
+	      	mockCaseService.commentText = 'test comment';
+	      	mockScope.saveDraftPromise = '3'
+	      	mockCaseService.kase.status = {
+	          name: 'Closed'
+	        };
+	        expect(mockScope.addComment).toBeDefined();
+	      	mockScope.addComment();
+	      	spyOn(mockStrataService.cases.comments, 'post').andCallThrough();  
+	     	mockScope.$root.$digest();  
+	      	expect(mockCaseService.kase.status.name).toEqual("Waiting on Red Hat");
+	  
+	  	}));
+
+	  	it('should have a function for adding comments to case rejected', inject(function ($controller) {
+
+	        $controller('AddCommentSection', {
+	          	$scope: mockScope,
+	          	CaseService: mockCaseService,
+	          	strataService: mockStrataService
+	        });
+
+	      	mockCaseService.kase.case_number = '1234';
+	      	mockCaseService.commentText = 'test comment';
+	      	mockScope.saveDraftPromise = '3'
+	      	mockCaseService.kase.status = {
+	          name: 'Closed'
+	        };
+	      	expect(mockScope.addComment).toBeDefined();
+	      	mockStrataService.rejectCalls();
+	      	spyOn(mockStrataService.cases.comments, 'post').andCallThrough();
+	      	mockScope.addComment();	      	  
+	     	mockScope.$root.$digest();  
+	      	expect(mockCaseService.kase.status.name).toEqual("Closed");
+	      	expect(mockScope.addingComment).toBe(false);
+	  
+	  	}));
+
+	  	it('should have a function for adding draft comments to case', inject(function ($controller) {
+
+		    $controller('AddCommentSection', {
+		        $scope: mockScope,
+		        CaseService: mockCaseService,
+		        strataService: mockStrataService
+		    });
+
+		    mockCaseService.kase.case_number = '1234';
+		    mockCaseService.commentText = 'test comment';
+		    mockScope.saveDraftPromise = '3'
+		    mockCaseService.kase.status = {
+	          name: 'Closed'
+	        };
+		    mockCaseService.draftComment = {};
+		    mockCaseService.draftComment.id = '1111';
+		    expect(mockScope.addComment).toBeDefined();
+		    mockScope.addComment();      
+		    spyOn(mockStrataService.cases.comments, 'put').andCallThrough();  
+		    mockScope.$root.$digest();        
+	        
+	  	}));
+
+	  	it('should have a function for saving non draft comments', inject(function ($controller) {
+
+		    $controller('AddCommentSection', {
+		        $scope: mockScope,
+		        CaseService: mockCaseService,
+		        strataService: mockStrataService
+		    });
+
+		    mockCaseService.kase.case_number = '1234';
+		    mockCaseService.commentText = 'test comment';
+		    expect(mockScope.saveDraft).toBeDefined();
+		    mockScope.saveDraft();      
+		    spyOn(mockStrataService.cases.comments, 'post').andCallThrough();  
+		    mockScope.$root.$digest(); 
+		    expect(mockScope.draftSaved).toBe(true);
+		    expect(mockCaseService.draftComment.case_number).toEqual('1234');       
+		    
 		}));
 
-		it('should have a function for adding comments to case', inject(function ($controller) {
+		it('should have a function for saving draft comments', inject(function ($controller) {
 
-				$controller('AddCommentSection', {
-						$scope: mockScope,
-						CaseService: caseService,
-						strataService: strataService
-				});
+		    $controller('AddCommentSection', {
+		        $scope: mockScope,
+		        CaseService: mockCaseService,
+		        strataService: mockStrataService
+		    });
 
-				caseService.kase.case_number = '1234';
-				caseService.commentText = 'test comment';
-				expect(mockScope.addComment).toBeDefined();
-				var deferred = q.defer();
-				spyOn(strataService.cases.comments, 'post').andReturn(deferred.promise);
-		        deferred.resolve();
-				mockScope.addComment();
-				expect(strataService.cases.comments.post).toHaveBeenCalledWith("1234", 'test comment');			
-
-		
+		    mockCaseService.kase.case_number = '1234';
+		    mockCaseService.commentText = 'test comment';
+		    mockCaseService.draftComment = {};
+		    expect(mockScope.saveDraft).toBeDefined();
+		    mockScope.saveDraft();      
+		    spyOn(mockStrataService.cases.comments, 'put').andCallThrough();  
+		    mockScope.$root.$digest(); 
+		    expect(mockScope.draftSaved).toBe(true);
+		    expect(mockCaseService.draftComment.text).toEqual('test comment');       
+		    
 		}));
 
-		it('should have a function for adding draft comments to case', inject(function ($controller) {
+		it('should have a function for saving draft comments rejected', inject(function ($controller) {
 
-				$controller('AddCommentSection', {
-						$scope: mockScope,
-						CaseService: caseService,
-						strataService: strataService
-				});
+		    $controller('AddCommentSection', {
+		        $scope: mockScope,
+		        CaseService: mockCaseService,
+		        strataService: mockStrataService
+		    });
 
-				caseService.kase.case_number = '1234';
-				caseService.commentText = 'test comment';
-				caseService.draftComment = {};
-				caseService.draftComment.id = '1111';
-				expect(mockScope.addComment).toBeDefined();
-				var deferred = q.defer();
-				spyOn(strataService.cases.comments, 'put').andReturn(deferred.promise);
-		        deferred.resolve();
-				mockScope.addComment();
-				expect(strataService.cases.comments.put).toHaveBeenCalledWith("1234", 'test comment', false, '1111');						
-
-		
+		    mockCaseService.kase.case_number = '1234';
+		    mockCaseService.commentText = 'test comment';
+		    mockCaseService.draftComment = {};
+		    expect(mockScope.saveDraft).toBeDefined();
+		    mockStrataService.rejectCalls();
+		    spyOn(mockStrataService.cases.comments, 'put').andCallThrough(); 
+		    mockScope.saveDraft();		     
+		    mockScope.$root.$digest(); 
+		    expect(mockScope.savingDraft).toBe(false);      
+		    
 		}));
 
-		it('should have a function for fetching recommendations', inject(function ($controller) {
+		it('should have a function for on New Comment Keypress', inject(function ($controller) {
 
-				$controller('New', {
-						$scope: mockScope,
-						RecommendationsService: recommendationsService,
-						strataService: strataService
-				});
+	        $controller('AddCommentSection', {
+	          	$scope: mockScope,
+	          	CaseService: mockCaseService,
+	          	strataService: mockStrataService
+	        });
 
-				var newData = {
-			        product: 'Red Hat Enterprise Linux',
-			        version: '6.0',
-			        summary: 'test case summary',
-			        description: 'test case description'		        
-      			};
+	        mockScope.addingComment = false;
+	        mockCaseService.commentText = 'test comment';
+	        expect(mockScope.onNewCommentKeypress).toBeDefined();
+	        mockScope.onNewCommentKeypress();           
+	        
+	  	}));
 
-      			caseService.kase.product = 'Red Hat Enterprise Linux';
-		        caseService.kase.version = '6.0';
-		        caseService.kase.summary = 'test case summary';
-		        caseService.kase.description = 'test case description';
+	});
 
-      			expect(mockScope.getRecommendations).toBeDefined();
-      			var deferred = q.defer();
-				spyOn(strataService, 'problems').andReturn(deferred.promise);
-		        deferred.resolve();
-				mockScope.getRecommendations();
-				expect(strataService.problems).toHaveBeenCalledWith(newData,5);						
+	//Suite for New
+	describe('New', function() {
 
-		
-		}));
+		it('should have a function for fetching recommendations resolved', inject(function ($controller) {
+
+	        $controller('New', {
+	            $scope: mockScope,
+	            RecommendationsService: mockRecommendationsService,
+	            SearchResultsService: mockSearchResultsService,
+	            strataService: mockStrataService
+	        });
+
+	        mockScope.NEW_CASE_CONFIG.showRecommendations = true;
+	        expect(mockScope.getRecommendations).toBeDefined();
+	        mockScope.getRecommendations();  
+	        spyOn(mockRecommendationsService, 'populateRecommendations').andCallThrough();
+	        mockScope.$root.$digest();          
+	        expect(mockSearchResultsService.results).toEqual(mockStrataDataService.mockSolutions); 
+
+  		}));
+
+  		it('should have a function for getting Product Versions resolved', inject(function ($controller) {
+
+	        $controller('New', {
+	            $scope: mockScope,
+	            CaseService: mockCaseService,
+	            RecommendationsService: mockRecommendationsService,
+	            SearchResultsService: mockSearchResultsService,
+	            strataService: mockStrataService,
+	            NEW_DEFAULTS: mockStrataDataService.value
+	        });
+
+	        var product = {
+	          name: 'Red Hat Enterprise Linux',
+	          code: '123456'
+	        };
+
+	        expect(mockScope.getProductVersions).toBeDefined();
+	        mockScope.getProductVersions(product);
+	        spyOn(mockStrataService.products, 'versions').andCallThrough();
+	        mockScope.$root.$digest();
+	        expect(mockCaseService.kase.version).toEqual(mockStrataDataService.value.version);
+
+  		}));
+
+  		it('should have a function for submitting case', inject(function ($controller) {
+
+	        $controller('New', {
+	            $scope: mockScope,
+	            CaseService: mockCaseService,
+	            RecommendationsService: mockRecommendationsService,
+	            SearchResultsService: mockSearchResultsService,
+	            strataService: mockStrataService,
+	            NEW_DEFAULTS: mockStrataDataService.value
+	        });
+
+			mockCaseService.kase.version = '6.0';
+	        mockCaseService.kase.summary = 'Test Summary';
+	        mockCaseService.kase.description = 'Test Description';
+	        mockCaseService.kase.severity = {
+	        	name: 'high',
+	        	value: '1'
+	        };
+	        mockCaseService.kase.product = {
+	          name: 'Red Hat Enterprise Linux',
+	          code: '123456'
+	        };
+	        mockCaseService.group = 'open';
+	        mockCaseService.entitlement = 'premium';
+	        mockCaseService.fts = true;
+	        mockCaseService.fts_contact = 'testUser@test.com';
+	        mockCaseService.owner = 'testUser';
+	        mockCaseService.kase.account = {
+	        	name: 'testAccount',
+	        	number: '12345'
+	        };	        
+
+	        expect(mockScope.doSubmit).toBeDefined();
+	        mockScope.doSubmit();
+	        spyOn(mockStrataService.cases, 'post').andCallThrough();
+	        mockScope.$root.$digest();
+	        expect(mockScope.submittingCase).toBe(false);
+
+  		}));
+
+	});	
+	
 	
 });
--- test/spec/cases/cases.directives.test.js
+++ test/spec/cases/cases.directives.test.js
@@ -2,40 +2,185 @@
 
 describe('Case Directives', function() {
 
-	var mockScope;
+    var mockScope;
     var compileService;
+    var mockStrataDataService;
     var securityService;
+    var caseService;
+    var recommendationsService;
+    var attachmentsService;
 
 
     beforeEach(angular.mock.module('RedhatAccess.cases'));
+    beforeEach(angular.mock.module('RedhatAccess.mock'));
+
     beforeEach(angular.mock.inject(function($rootScope, $compile) {
         mockScope = $rootScope.$new();
         compileService = $compile;
     }));
 
     beforeEach(function() {
-        inject(function($injector) {
+        inject(function($injector,_MockStrataDataService_,_RecommendationsService_,_AttachmentsService_,
+            _CaseService_) {
             securityService = $injector.get('securityService');
+            mockStrataDataService = _MockStrataDataService_;
+            recommendationsService = _RecommendationsService_;
+            attachmentsService = _AttachmentsService_;
+            caseService = _CaseService_;
         })
     });
 
-    it('should display the linked bugzilla section for internal user', function() {
-        var compileFn = compileService(' <div rha-listbugzillas/>');
-        var element = compileFn(mockScope);
-        securityService.loginStatus.isInternal = true;
-        mockScope.securityService = securityService;
-        mockScope.$root.$digest();
-        expect(element.find('.redhat-access-bz').length).toBe(1);        
+    //Suite for listBugzillas
+    describe('listBugzillas', function() {
+
+        it('should display the linked bugzilla section for internal user', function() {
+            var compileFn = compileService(' <div rha-listbugzillas/>');
+            var element = compileFn(mockScope);
+            securityService.loginStatus.isInternal = true;
+            mockScope.securityService = securityService;
+            mockScope.$root.$digest();
+            expect(element.find('.redhat-access-bz').length).toBe(1);        
+        });
+
+        it('should not display the linked bugzilla section for customer', function() {
+            var compileFn = compileService(' <div rha-listbugzillas/>');
+            var element = compileFn(mockScope);
+            securityService.loginStatus.isInternal = false;
+            mockScope.securityService = securityService;
+            mockScope.$root.$digest();
+            expect(element.find('.redhat-access-bz.ng-hide').length).toBe(1);        
+        });
+    });   
+
+    //Suite for detailsSection
+    describe('detailsSection', function() {
+
+        it('should display the accNo and Name details for internal user', function() {
+            var compileFn = compileService(' <div rha-casedetails/>');
+            var element = compileFn(mockScope);
+            securityService.loginStatus.isInternal = true;
+            mockScope.securityService = securityService;
+            mockScope.$root.$digest();
+            expect(element.find('.rha-detail-acc-name').length).toBe(1);
+            expect(element.find('.rha-detail-acc-number').length).toBe(1);
+
+        });
+
+        it('should not display the accNo and Name details for customer', function() {
+            var compileFn = compileService(' <div rha-casedetails/>');
+            var element = compileFn(mockScope);
+            securityService.loginStatus.isInternal = false;
+            mockScope.securityService = securityService;
+            mockScope.$root.$digest();
+            expect(element.find('.rha-detail-acc-name').length).toBe(0);
+            expect(element.find('.rha-detail-acc-number').length).toBe(0);
+
+        });
+
     });
 
-    it('should not display the linked bugzilla section for customer', function() {
-        var compileFn = compileService(' <div rha-listbugzillas/>');
-        var element = compileFn(mockScope);
-        securityService.loginStatus.isInternal = false;
-        mockScope.securityService = securityService;
-        mockScope.$root.$digest();
-        expect(element.find('.redhat-access-bz.ng-hide').length).toBe(1);        
+    //Suite for recommendationsSection
+    describe('recommendationsSection', function() {
+
+        it('should display spinner when recommendations are loading', function() {
+            var compileFn = compileService(' <div rha-caserecommendations/>');
+            var element = compileFn(mockScope);      
+            recommendationsService.loadingRecommendations = true; 
+            mockScope.RecommendationsService = recommendationsService;    
+            mockScope.$root.$digest();
+            expect(element.find('.rha-search-spinner-sm').length).toBe(1);
+            
+        });
+
+        it('should not display spinner when recommendations have loaded', function() {
+            var compileFn = compileService(' <div rha-caserecommendations/>');
+            var element = compileFn(mockScope);      
+            recommendationsService.loadingRecommendations = false; 
+            mockScope.RecommendationsService = recommendationsService;    
+            mockScope.$root.$digest();
+            expect(element.find('.rha-search-spinner-sm.ng-hide').length).toBe(1);
+                
+        });
+        
+        it('should have the correct pinned class', function() {
+            var compileFn = compileService(' <div rha-caserecommendations/>');
+            var element = compileFn(mockScope);            
+            mockScope.recommendationsOnScreen = [];
+            mockScope.RecommendationsService = recommendationsService;    
+            mockScope.$root.$digest();
+            expect(element.find('.pagination-sm').length).toBe(1);
+                
+        });
+
+    }); 
+
+    //Suite for listAttachments
+    describe('listAttachments', function() {
+
+        it('should display the no attachments div when list is empty', function() {
+            var compileFn = compileService(' <div rha-listattachments/>');
+            var element = compileFn(mockScope);            
+            attachmentsService.originalAttachments = [];
+            mockScope.AttachmentsService = attachmentsService;  
+            mockScope.$root.$digest();            
+            expect(element.find('.rha-attachments-section').length).toBe(1);
+            expect(element.find('.rha-attachments-section').text()).toEqual('No attachments added');
+                
+        });
+
+        it('should not display the no attachments div when attachments present', function() {
+            var compileFn = compileService(' <div rha-listattachments/>');
+            var element = compileFn(mockScope);            
+            attachmentsService.originalAttachments = mockStrataDataService.mockAttachments;
+            mockScope.AttachmentsService = attachmentsService;              
+            mockScope.$root.$digest();            
+            expect(element.find('.rha-attachments-section.ng-hide').length).toBe(1);            
+                
+        }); 
+
+    }); 
+
+    //Suite for commentsSection
+    describe('commentsSection', function() {
+
+        it('should display the comments section when comments present', function() {
+            var compileFn = compileService(' <div rha-casecomments/>');
+            var element = compileFn(mockScope);            
+            caseService.comments = mockStrataDataService.mockComments;
+            mockScope.CaseService = caseService;              
+            mockScope.$root.$digest();             
+            expect(element.find('.rha-comments-section').length).toBe(1);
+            expect(element.find('.rha-comment-text').text()).toEqual(mockStrataDataService.mockComments[0].text);
+                
+        }); 
+
+        it('should not display the comments section when no comments available', function() {
+            var compileFn = compileService(' <div rha-casecomments/>');
+            var element = compileFn(mockScope);            
+            caseService.comments = [];
+            mockScope.CaseService = caseService;              
+            mockScope.$root.$digest();            
+            expect(element.find('.rha-comments-section.ng-show').length).toBe(0);
+                
+        });
+
     });
+
+    //Suite for addCommentSection
+    describe('addCommentSection', function() {
+
+        it('should not display the comments section when no comments available', function() {
+            var compileFn = compileService(' <div rha-addcommentsection/>');
+            var element = compileFn(mockScope);            
+            caseService.commentText = 'hello';
+            mockScope.CaseService = caseService;
+            mockScope.addingComment = true; 
+            mockScope.$root.$digest();            
+            expect(element.find('.form-control').val()).toEqual('hello');
+
+        });
+
+    });                   
 
 
 });
