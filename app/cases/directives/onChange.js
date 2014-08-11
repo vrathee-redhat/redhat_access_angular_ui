3721dc18b133d66a1b701cb091ce192d401ef511
Spenser Shumaker <sshumake@redhat.com>
2014-08-11 10:10:40 -0400

    IE8ify local file attachment
    Spenser Shumaker <sshumake@redhat.com>

--- app/cases/directives/onChange.js
+++ app/cases/directives/onChange.js
@@ -2,11 +2,11 @@
 /*jshint unused:vars */
 
 angular.module('RedhatAccess.cases')
-.directive('rhaOnChange', function () {
+.directive('rhaOnchange', function () {
   return {
     restrict: 'A',
     link: function (scope, element, attrs) {
-      element.bind('change', element.scope()[attrs.rhaOnChange]);
+      element.bind('change', element.scope()[attrs.rhaOnchange]);
     }
   };
 });
