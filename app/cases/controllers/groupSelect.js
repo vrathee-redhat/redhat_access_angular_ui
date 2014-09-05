449608b6e6eb29b8ff79d20ce699ac9f2e414f9c
Connor Hindley <conn.hindley@gmail.com>
2014-09-05 16:49:19 -0400

    fix group loading in new case

--- app/cases/controllers/groupSelect.js
+++ app/cases/controllers/groupSelect.js
@@ -29,20 +29,19 @@ angular.module('RedhatAccess.cases').constant('CASE_GROUPS', {
                 });
 
                 var defaultGroup = '';
-                if (!$scope.showsearchoptions) {
-                    return;
+                if ($scope.showsearchoptions) {
+                    var sep = '────────────────────────────────────────';
+                    $scope.groupOptions.push({
+                        value: '',
+                        label: 'All Groups'
+                    }, {
+                        value: CASE_GROUPS.ungrouped,
+                        label: 'Ungrouped Cases'
+                    }, {
+                        isDisabled: true,
+                        label: sep
+                    });
                 }
-                var sep = '────────────────────────────────────────';
-                $scope.groupOptions.push({
-                    value: '',
-                    label: 'All Groups'
-                }, {
-                    value: CASE_GROUPS.ungrouped,
-                    label: 'Ungrouped Cases'
-                }, {
-                    isDisabled: true,
-                    label: sep
-                });
 
                 angular.forEach(groups, function(group){
                     $scope.groupOptions.push({
@@ -53,14 +52,15 @@ angular.module('RedhatAccess.cases').constant('CASE_GROUPS', {
                         defaultGroup = group;
                     }
                 });
-
-                $scope.groupOptions.push({
-                    isDisabled: true,
-                    label: sep
-                }, {
-                    value: CASE_GROUPS.manage,
-                    label: 'Manage Case Groups'
-                });
+                if ($scope.showsearchoptions) {
+                    $scope.groupOptions.push({
+                        isDisabled: true,
+                        label: sep
+                    }, {
+                        value: CASE_GROUPS.manage,
+                        label: 'Manage Case Groups'
+                    });
+                }
                 // Assign to default group or '' which will fallback
                 // to 'All Groups'
                 $scope.CaseService.group = defaultGroup;
