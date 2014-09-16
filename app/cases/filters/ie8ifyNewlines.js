369793c6c9798a6d71729a5434e54510487268c2
Spenser Shumaker <sshumake@redhat.com>
2014-09-16 14:53:38 -0400

    Remove ie8ify and give up
    Spenser Shumaker <sshumake@redhat.com>

--- app/cases/filters/ie8ifyNewlines.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-angular.module('RedhatAccess.cases').filter('ie8ifyNewlines', function () {
-    return function (text) {
-        text.replace(/\n/g, '\r\n');
-        return text;
-    };
-});
