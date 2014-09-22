74ae703637b6fce192b5a5830b72aa1fe727bdc0
Spenser Shumaker <sshumake@redhat.com>
2014-09-22 10:36:03 -0400

    Add double click support for files
    Spenser Shumaker <sshumake@redhat.com>

--- app/log_viewer/controllers/fileController.js
+++ app/log_viewer/controllers/fileController.js
@@ -1,9 +1,15 @@
 'use strict';
 angular.module('RedhatAccess.logViewer').controller('fileController', [
     '$scope',
+    '$rootScope',
+    '$http',
+    '$location',
     'files',
-    function ($scope, files) {
+    'AlertService',
+    'LOGVIEWER_EVENTS',
+    function ($scope, $rootScope, $http, $location, files, AlertService, LOGVIEWER_EVENTS) {
         $scope.roleList = '';
+        $scope.retrieveFileButtonIsDisabled = files.getRetrieveFileButtonIsDisabled();
         $scope.$watch(function () {
             return $scope.mytree.currentNode;
         }, function () {
@@ -18,6 +24,30 @@ angular.module('RedhatAccess.logViewer').controller('fileController', [
             return files.fileList;
         }, function () {
             $scope.roleList = files.fileList;
+        });
+
+        $scope.selectItem = function(){
+            if(files.selectedFile !== undefined && !files.getRetrieveFileButtonIsDisabled()){
+                $scope.fileSelected();
+            }
+        }
+
+        $scope.fileSelected = function () {
+            files.setFileClicked(true);
+            var sessionId = $location.search().sessionId;
+            var userId = $location.search().userId;
+            $scope.$parent.$parent.sidePaneToggle = !$scope.$parent.$parent.sidePaneToggle;
+            $http({
+                method: 'GET',
+                url: 'logs?sessionId=' + encodeURIComponent(sessionId) + '&userId=' + encodeURIComponent(userId) + '&path=' + files.selectedFile + '&machine=' + files.selectedHost
+            }).success(function (data, status, headers, config) {
+                files.file = data;
+            }).error(function (data, status, headers, config) {
+                AlertService.addDangerMessage(data);
+            });
+        };
+        $rootScope.$on(LOGVIEWER_EVENTS.allTabsClosed, function () {
+            $scope.$parent.$parent.sidePaneToggle = !$scope.$parent.$parent.sidePaneToggle;
         });
     }
 ]);
