6f890b08108b7a453e96d5d46893cfda4afb5033
Aritro Dasgupta <ardasgup@redhat.com>
2014-09-23 12:34:53 +0530

    Fix for Adding comments should have a 32K character limit

--- app/cases/directives/addCommentSection.js
+++ app/cases/directives/addCommentSection.js
@@ -3,6 +3,23 @@ angular.module('RedhatAccess.cases').directive('rhaAddcommentsection', function 
     return {
         templateUrl: 'cases/views/addCommentSection.html',
         restrict: 'A',
-        controller: 'AddCommentSection'
+        controller: 'AddCommentSection',
+        link: function (scope, element, attr) {
+        	scope.maxCharacterCheck = function() {
+        		element.ready(function() {
+                    $('#case-comment-box').attr('maxlength', '32000');
+                    var textareaValue = $("#case-comment-box").val();
+					var maxlength = '32000';
+					if(maxlength > textareaValue.length) {
+						var count = textareaValue.length * 100 / maxlength;
+						parseInt(count);
+						$('#commentNotice .progressCount').html( (Math.round(count * 100) / 100) + '%' );
+						$('#commentNotice .progressBar').css({
+							'width' : count + '%'
+						});
+					}	
+                });
+        	};
+        }	
     };
 });
