0078f5be3012dc1673e106798e5ea19e3b14352f
Spenser Shumaker <sshumake@redhat.com>
2014-10-27 17:00:43 -0400

    IE8 Test

--- app/cases/filters/IE8preFix.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-angular.module('RedhatAccess.cases').filter('IE8preFix', function () {
-    return function (text) {
-        text = '<pre>' + text + '</pre>'
-        return text;
-    };
-});
