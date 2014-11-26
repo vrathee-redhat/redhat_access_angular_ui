c917baf2319b10943ff6ac8cbded89485754b007
Aritro Dasgupta <ardasgup@redhat.com>
2014-11-26 19:37:38 +0530

    reverted the isPCM flag

--- app/cases/cases.module.js
+++ app/cases/cases.module.js
@@ -34,7 +34,7 @@ angular.module('RedhatAccess.cases', [
     'showAttachments': true,
     'showServerSideAttachments': true,
     'productSortListFile': '/productSortList.txt',
-    'isPCM': true
+    'isPCM': false
 }).value('EDIT_CASE_CONFIG', {
     'showDetails': true,
     'showDescription': true,
@@ -44,7 +44,7 @@ angular.module('RedhatAccess.cases', [
     'showComments': true,
     'showServerSideAttachments': true,
     'showEmailNotifications': true,
-    'isPCM': true
+    'isPCM': false
 }).config([
     '$stateProvider',
     function($stateProvider) {
