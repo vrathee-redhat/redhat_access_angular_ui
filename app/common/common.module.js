d0ce12ce8e6cb90880d38470fabe1dc28e563545
Spenser Shumaker <sshumake@redhat.com>
2014-12-04 15:20:11 -0500

    Configurable headers

--- app/common/common.module.js
+++ app/common/common.module.js
@@ -13,7 +13,8 @@ angular.module('RedhatAccess.common', [
     'sfdcOutageMessage': '<ul class="message"><li class="alertSystem">Creating and updating support cases online is currently disabled. Please <a target="_blank" href="https://access.redhat.com/support/contact/technicalSupport/">contact Red Hat support</a> if you need immediate assistance.</li></ul>',
     'doSfdcHealthCheck' : false,
     'sfdcIsHealthy': true, // This property should be made false only when 'doSfdcHealthCheck' is set to false
-    'healthCheckInterval': 60000
+    'healthCheckInterval': 60000,
+    'showTitle': true
 }).factory('configurationService', [
 	'$q',
 	function($q) {
