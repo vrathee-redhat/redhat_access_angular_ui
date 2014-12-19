3feb24431ea04297d160d82917f9b589a2761305
Renu Jhamtani <rjhamtan@rjhamtan.csb>
2014-12-19 15:56:45 +0530

    Changes for PCM 404

--- app/cases/controllers/commentsSection.js
+++ app/cases/controllers/commentsSection.js
@@ -5,24 +5,52 @@ angular.module('RedhatAccess.cases').controller('CommentsSection', [
     '$scope',
     'CaseService',
     'strataService',
+    'securityService',
     '$stateParams',
+    '$rootScope',
+    'AUTH_EVENTS',
     'AlertService',
     '$modal',
     '$location',
     '$anchorScroll',
     'RHAUtils',
-    function ($scope, CaseService, strataService, $stateParams, AlertService, $modal, $location, $anchorScroll, RHAUtils) {
+    function ($scope, CaseService, strataService,securityService, $stateParams,$rootScope,AUTH_EVENTS, AlertService, $modal, $location, $anchorScroll, RHAUtils) {
         $scope.CaseService = CaseService;
         $scope.ie8 = window.ie8;
         $scope.ie9 = window.ie9;
 
-        CaseService.populateComments($stateParams.id).then(function (comments) {
-            $scope.$on('rhaCaseSettled', function() {
-                $scope.$evalAsync(function() {
-                    CaseService.scrollToComment($location.search().commentId);
+        $scope.authLoginEvent = $rootScope.$on(AUTH_EVENTS.loginSuccess, function () {
+            $scope.firePageLoadEvent();
+            CaseService.populateComments($stateParams.id).then(function (comments) {
+                $scope.$on('rhaCaseSettled', function() {
+                    $scope.$evalAsync(function() {
+                        CaseService.scrollToComment($location.search().commentId);
+                    });
                 });
             });
         });
+
+        $scope.firePageLoadEvent = function () {
+            if (window.chrometwo_require !== undefined) {
+                chrometwo_require(['analytics/attributes', 'analytics/main'], function(attrs, paf) {
+                    attrs.harvest();
+                    paf.report();
+                });
+            }
+        };
+
+        if (securityService.loginStatus.isLoggedIn) {
+            $scope.firePageLoadEvent();
+            CaseService.populateComments($stateParams.id).then(function (comments) {
+                $scope.$on('rhaCaseSettled', function() {
+                    $scope.$evalAsync(function() {
+                        CaseService.scrollToComment($location.search().commentId);
+                    });
+                });
+            });
+        }
+
+
         $scope.requestManagementEscalation = function () {
             $modal.open({
                 templateUrl: 'cases/views/requestManagementEscalationModal.html',
