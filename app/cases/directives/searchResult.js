05f056d0c59cc23bcf2bfae21016d24210961a55
Spenser Shumaker <sshumake@redhat.com>
2015-02-09 10:36:56 -0500

    Code refactoring

--- app/cases/directives/searchResult.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('RedhatAccess.cases').directive('rhaCasesearchresult', function () {
-    return {
-        templateUrl: 'cases/views/searchResult.html',
-        restrict: 'A',
-        scope: { theCase: '=case' }
-    };
-});
