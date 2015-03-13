27cf715598b534c3e5bd75793b261b139c72f2d4
Spenser Shumaker <sshumake@redhat.com>
2015-03-13 12:47:13 -0400

    Add missing changes

--- app/escalation/directives/403.js
+++ app/escalation/directives/403.js
@@ -2,7 +2,7 @@
 /*jshint unused:vars */
 angular.module('RedhatAccess.escalation').directive('rha403escalationerror', function () {
     return {
-        templateUrl: 'escallation/views/403.html',
+        templateUrl: 'escalation/views/403.html',
         restrict: 'A'
     };
 });
