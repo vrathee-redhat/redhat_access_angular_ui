f2289dbdbbb5a726bce53c94c1df6fcb1f744ff7
rjhamtan <Bhavna$03>
2015-04-16 10:44:28 +0530

    UsePhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -102,7 +102,7 @@ module.exports = function(config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ['Firefox'],
+        browsers: ['PhantomJS'],
 
 
         // Continuous Integration mode
