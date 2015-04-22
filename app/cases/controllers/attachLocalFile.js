3fe5009dac7a7e212a671805c7ad109f2de6c635
rjhamtan <Bhavna$03>
2015-04-22 19:33:00 +0530

    PCM-908 changes

--- app/cases/controllers/attachLocalFile.js
+++ app/cases/controllers/attachLocalFile.js
@@ -50,7 +50,7 @@ angular.module('RedhatAccess.cases').controller('AttachLocalFile', [
                 $scope.$apply();
 	            $scope.addFile();
             } else {
-                AlertService.addDangerMessage($('#fileUploader')[0].files[0].name + ' '+translate('cannot be attached because it is larger than') +''+ (AttachmentsService.maxAttachmentSize/1024) + ' '+ translate('GB. Please FTP large files to dropbox.redhat.com.'));
+                AlertService.addDangerMessage($('#fileUploader')[0].files[0].name + ' '+translate('cannot be attached because it is larger than') +' '+ (AttachmentsService.maxAttachmentSize/1024) + ' '+ translate('GB. Please FTP large files to dropbox.redhat.com.'));
             }
             $('#fileUploader')[0].value = '';
         };
