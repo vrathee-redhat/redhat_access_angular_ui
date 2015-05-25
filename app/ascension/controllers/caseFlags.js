6303b84cce897de95f0274f66ffb03861a131cc6
Aritro Dasgupta <ardasgup@redhat.com>
2015-05-25 12:21:01 +0530

    Fix for PCM-865

--- app/ascension/controllers/caseFlags.js
+++ app/ascension/controllers/caseFlags.js
@@ -2,7 +2,22 @@
 /*jshint camelcase: false */
 angular.module('RedhatAccess.ascension').controller('CaseFlags', [
     '$scope',
-    function ($scope) {
+    'CaseDetailsService',
+    'securityService',
+    'AUTH_EVENTS',
+    function ($scope, CaseDetailsService, securityService, AUTH_EVENTS) {
+    	$scope.CaseDetailsService = CaseDetailsService;
+    	
+    	$scope.init = function () {
 
+        };
+        
+        if (securityService.loginStatus.isLoggedIn) {
+            $scope.init();
+        }
+
+        $scope.$on(AUTH_EVENTS.loginSuccess, function () {
+            $scope.init();
+        });
     }
 ]);
