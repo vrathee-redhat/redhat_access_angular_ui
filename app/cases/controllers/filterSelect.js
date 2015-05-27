1d6fc6ead768a2f55603339bece8a69ba4181c41
Spenser Shumaker <sshumake@redhat.com>
2015-05-27 12:53:51 -0400

    Revert "Revert "Changes for PCM-342""

--- app/cases/controllers/filterSelect.js
+++ app/cases/controllers/filterSelect.js
@@ -6,51 +6,20 @@ angular.module('RedhatAccess.cases').controller('FilterSelect', [
     'CaseService',
     'STATUS',
     'translate',
-    function ($scope, securityService, CaseService, STATUS, translate) {
+    'ConstantsService',
+    function ($scope, securityService, CaseService, STATUS, translate, ConstantsService) {
         $scope.securityService = securityService;
         $scope.CaseService = CaseService;
+        $scope.ConstantsService = ConstantsService;
         $scope.STATUS = STATUS;
-        $scope.statuses = [
-            {
-                name: translate('Newest Date Modified'),
-                sortField: 'lastModifiedDate',
-                sortOrder: 'DESC'
-            },
-            {
-                name: translate('Oldest Date Modified'),
-                sortField: 'lastModifiedDate',
-                sortOrder: 'ASC'
-            },
-            {
-                name: translate('Highest Severity'),
-                sortField: 'severity',
-                sortOrder: 'ASC'
-            },
-            {
-                name: translate('Lowest Severity'),
-                sortField: 'severity',
-                sortOrder: 'DESC'
-            },
-            {
-                name: translate('Newest Date Created'),
-                sortField: 'createdDate',
-                sortOrder: 'DESC'
-            },
-            {
-                name: translate('Oldest Date Created'),
-                sortField: 'createdDate',
-                sortOrder: 'ASC'
-            },
-            {
-                name: translate('Case Owner (A-Z)'),
-                sortField: 'owner',
-                sortOrder: 'DESC'
-            },
-            {
-                name: translate('Case Owner (Z-A)'),
-                sortField: 'owner',
-                sortOrder: 'ASC'
+        $scope.init = function() {
+            CaseService.filterSelect = ConstantsService.sortByParams[0];
+            if(CaseService.localStorageCache) {
+                if (CaseService.localStorageCache.get('filterSelect'+securityService.loginStatus.authedUser.sso_username)) {
+                    var filterSelectCache = CaseService.localStorageCache.get('filterSelect'+securityService.loginStatus.authedUser.sso_username);
+                    CaseService.setFilterSelectModel(filterSelectCache.sortField,filterSelectCache.sortOrder);
+                }
             }
-        ];
+        };
     }
 ]);
