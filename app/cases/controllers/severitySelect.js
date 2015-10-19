22f052f88a1ccec64c8bf4a174a3dd0ed4eb1060
Jonáš Trantina <jtrantin@redhat.com>
2015-10-19 09:46:22 +0200

    Fix for PCM-1406

--- app/cases/controllers/severitySelect.js
+++ app/cases/controllers/severitySelect.js
@@ -3,13 +3,18 @@ angular.module('RedhatAccess.cases').controller('SeveritySelect', [
     '$scope',
     function ($scope) {
     	// INIT
-    	$scope.openedDetails = {};
+        $scope.openedDetails = {};
     	angular.forEach($scope.severities, function(severity) {
     		$scope.openedDetails[severity.name] = false;
     	});
 
-        $scope.toggleDetails = function (severity) {
+        $scope.toggleDetails = function (severity, event) {
+            event.stopPropagation(); // we don't want to toggle severity
         	$scope.openedDetails[severity.name] = !$scope.openedDetails[severity.name];
+        };
+
+        $scope.toggleSeverity = function (severity) {
+            $scope.severityModel = severity;
         };
 
         $scope.$watch("severityModel", function() {
