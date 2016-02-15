14613860c99218f397a51449694c1160d1c0c780
Jonáš Trantina <jtrantin@redhat.com>
2016-02-15 11:17:58 +0100

    PCM-1694: Switched from /rs/problems to /rs/recommendations

--- app/cases/services/recommendationsService.js
+++ app/cases/services/recommendationsService.js
@@ -80,23 +80,20 @@ angular.module('RedhatAccess.cases').service('RecommendationsService', [
 
                 if ((newData.product !== undefined || newData.version !== undefined || newData.summary !== undefined || newData.description !== undefined || (!angular.equals(currentData, newData) && !this.loadingRecommendations))) {
                     this.loadingRecommendations = true;
-                    currentData = newData
-                    strataService.recommendationsXmlHack(currentData, max, true, '%3Cspan%20class%3D%22recommendationsKeywords%22%3E%2C%3C%2Fspan%3E').then(angular.bind(this, function (solutions) {
+                    currentData = newData;
+                    strataService.recommendationsForCase(currentData, max, 0).then(angular.bind(this, function (response) {
                         //retrieve details for each solution
                         if(refreshRecommendations){
                             this.recommendations = [];
                         }
-                        solutions.forEach(angular.bind(this, function (solution) {
+                        response.docs.forEach(angular.bind(this, function (solution) {
                             if (solution !== undefined) {
                                 solution.resource_type = 'Solution';
-                                try {
-                                    solution.abstract = $sanitize(solution.abstract);
-                                }
-                                catch(err) {
-                                    solution.abstract = '';
-                                }
+                                solution.resource_id = solution.id;
+                                solution.resource_view_uri = solution.view_uri;
+                                solution.title = solution.allTitle;
                                 //this is to sync the case detail pinned recommendation with /rs/problems recommendation w.r.t pinned flag so that red pin will appear in both section
-                                var pinnedRecommendation = $filter('filter')(self.pinnedRecommendations, function (rec) {return rec.resource_id === solution.resource_id;})[0];
+                                var pinnedRecommendation = $filter('filter')(self.pinnedRecommendations, function (rec) {return rec.resource_id === solution.id;})[0];
                                 if (RHAUtils.isNotEmpty(pinnedRecommendation)) {
                                     if (pinnedRecommendation.pinned_at) {
                                         solution.pinned = true;
