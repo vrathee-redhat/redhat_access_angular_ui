67d1146c0495afd27ad6a41a5a7acb704e5c82e3
Jonáš Trantina <jtrantin@redhat.com>
2016-04-28 16:57:24 +0200

    PCM-1980: Added button navigating to account bookmark view

--- app/cases/controllers/listFilter.js
+++ app/cases/controllers/listFilter.js
@@ -11,7 +11,8 @@ angular.module('RedhatAccess.cases').controller('ListFilter', [
     'GroupService',
     'ConstantsService',
     'RHAUtils',
-    function ($scope, STATUS, CaseService, securityService, $rootScope, CASE_EVENTS, SearchCaseService, GroupService, ConstantsService, RHAUtils) {
+    '$state',
+    function ($scope, STATUS, CaseService, securityService, $rootScope, CASE_EVENTS, SearchCaseService, GroupService, ConstantsService, RHAUtils, $state) {
         $scope.securityService = securityService;
         $scope.CaseService = CaseService;
         $scope.GroupService = GroupService;
@@ -19,9 +20,11 @@ angular.module('RedhatAccess.cases').controller('ListFilter', [
         $scope.ConstantsService = ConstantsService;
         CaseService.status = STATUS.open;
         $scope.showsearchoptions = CaseService.showsearchoptions;
+        $scope.bookmarkAccountUrl = $state.href('accountBookmark');
+
         $scope.doSearch = function(){
             $rootScope.$broadcast(CASE_EVENTS.searchSubmit);
-        }
+        };
         $scope.setSearchOptions = function (showsearchoptions) {
             CaseService.showsearchoptions = showsearchoptions;
             CaseService.buildGroupOptions();            
