d4920a4ea22413f50969e6c602de42a177f3dd60
Kun Yan <kyan@redhat.com>
2017-04-07 16:33:30 +0800

    PCM-4396 Selected managed account name must be displayed in dropdown once navigate back to case list page from other pages

--- app/cases/controllers/managedAccountSelect.js
+++ app/cases/controllers/managedAccountSelect.js
@@ -1,11 +1,8 @@
 'use strict';
 
-import {isEmpty,forEach} from 'lodash'
-
 export default class ManagedAccountSelect {
     constructor(securityService) {
         'ngInject';
         this.securityService = securityService;
-        this.selectedAccount = '';
     }
 }
