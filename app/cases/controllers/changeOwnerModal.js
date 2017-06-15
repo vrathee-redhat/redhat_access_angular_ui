636ee10e2e5f3247dad5c5e26c4df316bc5fbc38
kgandhi <kgandhi@redhat.com>
2017-06-15 19:50:48 +0530

    pcm-4969 and pcm-4970 code changes

--- app/cases/controllers/changeOwnerModal.js
+++ app/cases/controllers/changeOwnerModal.js
@@ -7,16 +7,21 @@ export default class ChangeOwnerModal {
         $scope.CaseService = CaseService;
         $scope.caseOwner = {};
         $scope.RHAUtils = RHAUtils;
+        $scope.updatingOwner = false;
         $scope.confirm = () => {
             if (RHAUtils.isNotEmpty($scope.caseOwner.sso_username)) {
+                $scope.updatingOwner = true;
                 let promise = strataService.cases.owner.update(CaseService.kase.case_number,$scope.caseOwner.sso_username);
                 promise.then((response) => {
                     AlertService.addSuccessMessage(gettextCatalog.getString('Case  {{caseNumber}} owner successfully changed.', {caseNumber: CaseService.kase.case_number}));
                     CaseService.kase.owner = $scope.caseOwner.first_name+' '+$scope.caseOwner.last_name;
                     angular.copy(CaseService.kase, CaseService.prestineKase);
+                    $scope.updatingOwner = false;
                     $uibModalInstance.close();
                 }, (error) => {
+                    $scope.updatingOwner = false;
                     AlertService.addStrataErrorMessage(error);
+                    $uibModalInstance.close();
                 });
             }
         };
