72f8642375a0c3ce40e1b5542891adf7c7a84874
Samuel Mendenhall <Samuel.Mendenhall@gmail.com>
2018-03-14 15:47:45 -0400

    using regex

--- .openshift/server.js
+++ .openshift/server.js
@@ -20,6 +20,8 @@ function setCustomCacheControl(res, path) {
     }
 }
 
+const iceProxyRe = /(ext\.paas\.(?:\w+).redhat.com)/;
+
 app
     .use(morgan('combined')) // logger
     .use(compression()) // gzip
@@ -28,8 +30,8 @@ app
         res.send('<h1>Application is alive :)</h1>');
     })
     .get('/support/cases/ice', function(req, res, next) {
-        var hostname = req.headers.host.split('-')[1];
-        var newUrl = 'http://ice-' + hostname + '/ice';
+        var hostname = iceProxyRe.exec(req.headers.host);
+        var newUrl = 'http://ice-' + hostname[0] + '/ice';
         req.pipe(request(newUrl)).pipe(res);
     })
     .get('/*', function(req, res, next) {
