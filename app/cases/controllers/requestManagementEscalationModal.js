0a5e595000df27d57bc2e9f140cc4ce199d3c994
Anuj Singla <asingla@redhat.com>
2018-05-02 19:19:28 -0400

    WIP - PCM-5591 .

--- app/cases/controllers/requestManagementEscalationModal.js
+++ app/cases/controllers/requestManagementEscalationModal.js
@@ -1,11 +1,17 @@
 'use strict';
 
+// import hydrajs  from '../../shared/hydrajs.js';
+
 export default class RequestManagementEscalationModal {
-    constructor($scope, $uibModalInstance, AlertService, CaseService, DiscussionService, strataService, securityService, $q, $stateParams, RHAUtils) {
+    constructor($scope, $uibModalInstance, AlertService, CaseService, DiscussionService, strataService, securityService, $q, $stateParams, RHAUtils, gettextCatalog) {
         'ngInject';
 
         $scope.CaseService = CaseService;
         $scope.submittingRequest = false;
+        $scope.escalationSubject = '';
+        $scope.escalationDescription = '';
+        $scope.escalationExpectations = '';
+        $scope.securityService = securityService;
         if (RHAUtils.isNotEmpty(CaseService.commentText)) {
             $scope.disableSubmitRequest = false;
             CaseService.escalationCommentText = CaseService.commentText;
@@ -65,11 +71,52 @@ export default class RequestManagementEscalationModal {
             $uibModalInstance.close();
         };
         $scope.onNewEscalationComment = function () {
-            if (RHAUtils.isNotEmpty(CaseService.escalationCommentText) && !$scope.submittingRequest) {
+            if (RHAUtils.isNotEmpty($scope.escalationSubject) && !$scope.submittingRequest && RHAUtils.isNotEmpty($scope.escalationDescription)
+        && RHAUtils.isNotEmpty($scope.escalationExpectations)) {
                 $scope.disableSubmitRequest = false;
             } else if (RHAUtils.isEmpty(CaseService.escalationCommentText)) {
                 $scope.disableSubmitRequest = true;
             }
+        };
+
+        $scope.submitRMEEscalation = function () {
+            // hydrajs.users.getUserBySSO(CaseService.kase.contact_sso_username).then((u) => {
+            //     console.log(u);
+            // });
+            $scope.submittingRequest = true;
+            let escalationJSON = {
+                'record_type': 'Active Customer Escalation',
+                'subject': $scope.escalationSubject,
+                'issue_description': $scope.escalationDescription,
+                'escalation_source': 'RME Escalation',
+                'expectations': $scope.escalationExpectations,
+                'status': 'New',
+                'account_number': CaseService.kase.account_number,
+                'case_number': CaseService.kase.case_number,
+                'customer_name':  securityService.loginStatus.authedUser.loggedInUser,
+                'customer_email': securityService.loginStatus.authedUser.email,
+                'customer_phone': securityService.loginStatus.authedUser.phone_number,
+                'requestor': securityService.loginStatus.authedUser.loggedInUser,
+                'requestor_email': securityService.loginStatus.authedUser.email,
+                'requestor_phone': securityService.loginStatus.authedUser.phone_number,
+                'already_escalated': false,
+                'geo': 'APAC',
+                'severity': '3',
+            };
+            const promise = strataService.escalationRequest.create(escalationJSON);
+            promise.then((escalationNum) => {
+                AlertService.clearAlerts();
+                $scope.closeModal();
+                $scope.submittingRequest = false;  
+                if (escalationNum !== undefined) {       
+                    AlertService.addSuccessMessage(gettextCatalog.getString('Your Escalation request has been sent successfully'));
+                }
+            }, (error) => {
+                AlertService.clearAlerts();
+                $scope.closeModal();
+                $scope.submittingRequest = false;  
+                AlertService.addStrataErrorMessage(error);
+            });
         };
     }
 }
