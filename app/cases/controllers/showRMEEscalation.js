a0c7e898f591f444161fccbeb54159e7835b6e27
Anuj Singla <asingla@redhat.com>
2018-06-14 16:57:45 +0530

    code changes.

---
+++ app/cases/controllers/showRMEEscalation.js
@@ -0,0 +1,10 @@
+'use strict';
+
+export default class ShowRmeEscalation {
+    constructor($scope, RHAUtils, CaseService) {
+        'ngInject';
+
+        $scope.CaseService = CaseService;
+
+    }
+}
---
+++ app/cases/directives/showRMEEscalation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+export default function () {
+    return {
+        template: require('../views/showRMEEscalation.jade'),
+        restrict: 'A',
+        controller: 'ShowRmeEscalation'
+    };
+}
