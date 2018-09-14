299143eea550fbb1944804c5ddc0d9cdfe662124
jeudy <jeudy@redhat.com>
2018-09-14 13:41:49 -0400

    fixed review changes

--- app/cases/controllers/discussionSection.js
+++ app/cases/controllers/discussionSection.js
@@ -52,6 +52,7 @@ export default class DiscussionSection {
                 if ($location.search().commentId !== undefined) {
                     scroll($location.search().commentId);
                 }
+                console.log(DiscussionService.discussionElements);
             }, function (error) {
             }));
         };
@@ -280,5 +281,7 @@ export default class DiscussionSection {
                 AlertService.addDangerMessage(error);
             });
         };
+
+        $scope.temp = (element) => console.log(element);
     }
 }
