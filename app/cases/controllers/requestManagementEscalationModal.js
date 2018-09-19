6eeac236cf6e9057baf2cedd10801cb052b1ff44
Anuj Singla <asingla@redhat.com>
2018-09-19 15:56:25 +0530

    PCM-7233 - Expose escalation closed dates to customers .

--- app/cases/controllers/requestManagementEscalationModal.js
+++ app/cases/controllers/requestManagementEscalationModal.js
@@ -15,60 +15,7 @@ export default class RequestManagementEscalationModal {
             {label:'Asia Pacific', value: 'APAC'},{label:'Combo', value: 'Combo'}
         ];
         $scope.securityService = securityService;
-        if (RHAUtils.isNotEmpty(CaseService.commentText)) {
-            $scope.disableSubmitRequest = false;
-            CaseService.escalationCommentText = CaseService.commentText;
-        } else {
-            $scope.disableSubmitRequest = true;
-        }
-        $scope.submitRequestClick = angular.bind($scope, function (commentText) {
-            $scope.submittingRequest = true;
-            var fullComment = 'Request Management Escalation: ' + commentText;
-            var onSuccess = function (response) {
-                var caseJSON = {'escalated': true};
-                var updateCase = strataService.cases.put(CaseService.kase.case_number, caseJSON);
-                updateCase.then(function (response) {
-                    CaseService.checkForCaseStatusToggleOnAttachOrComment();
-                }, function (error) {
-                    AlertService.addStrataErrorMessage(error);
-                });
-
-                CaseService.populateComments($stateParams.id).then(function (comments) {
-                    $scope.closeModal();
-                    if (CaseService.localStorageCache) {
-                        CaseService.localStorageCache.remove(CaseService.kase.case_number + securityService.loginStatus.authedUser.sso_username);
-                    }
-                    $scope.submittingRequest = false;
-                    CaseService.draftSaved = false;
-                    CaseService.draftComment = undefined;
-                    CaseService.commentText = undefined;
-                    DiscussionService.commentTextBoxEnlargen = false;
-                }, function (error) {
-                    AlertService.addStrataErrorMessage(error);
-                });
-            };
-            var onError = function (error) {
-                AlertService.addStrataErrorMessage(error);
-            };
-
-            if (CaseService.localStorageCache) {
-                if (CaseService.draftCommentOnServerExists) {
-                    strataService.cases.comments.put(CaseService.kase.case_number, fullComment, false, true, CaseService.draftComment.id).then(onSuccess, onError);
-                }
-                else {
-                    strataService.cases.comments.post(CaseService.kase.case_number, fullComment, true, false).then(onSuccess, onError);
-                }
-            }
-            else {
-                if (RHAUtils.isNotEmpty(CaseService.draftComment)) {
-                    strataService.cases.comments.put(CaseService.kase.case_number, fullComment, false, true, CaseService.draftComment.id).then(onSuccess, onError);
-                } else {
-                    strataService.cases.comments.post(CaseService.kase.case_number, fullComment, true, false).then(onSuccess, onError);
-                }
-            }
-
-
-        });
+        $scope.disableSubmitRequest = true;
         $scope.closeModal = function () {
             CaseService.escalationCommentText = undefined;
             CaseService.escalationSubject = undefined;
@@ -87,8 +34,6 @@ export default class RequestManagementEscalationModal {
             if (RHAUtils.isNotEmpty(CaseService.escalationSubject) && !$scope.submittingRequest && RHAUtils.isNotEmpty(CaseService.escalationDescription)
                && RHAUtils.isNotEmpty(CaseService.escalationExpectations) && RHAUtils.isNotEmpty(CaseService.rmeEscalationGeo)) {
                 $scope.disableSubmitRequest = false;
-            } else if (RHAUtils.isEmpty(CaseService.escalationCommentText)) {
-                $scope.disableSubmitRequest = true;
             }
         };
 
