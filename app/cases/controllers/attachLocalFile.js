0716e7dda49ab12770bf910a2d9d21291a7024f3
Keyur Gandhi <kgandhi@redhat.com>
2018-09-24 21:13:39 +0530

    PCM-7398 updated function name which checks for valid s3 account before tooltip is configured

--- app/cases/controllers/attachLocalFile.js
+++ app/cases/controllers/attachLocalFile.js
@@ -12,7 +12,7 @@ export default class AttachLocalFile {
 
         $scope.$watch('CaseService.account.number', function() {
             if(CaseService.account && RHAUtils.isNotEmpty(CaseService.account.number)) {
-                $scope.attachFileTT = AttachmentsService.isValidS3Upload() ? 'Can now accept large attachments (~5TB)' : ''
+                $scope.attachFileTT = AttachmentsService.isValidS3UploadAccount() ? 'Can now accept large attachments (~5TB)' : '';
             }
         });
 
