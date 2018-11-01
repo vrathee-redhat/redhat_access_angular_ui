c6c57aa129afdd21834b13cdec7e35a4072aeecf
jeudy <jeudy@redhat.com>
2018-11-01 15:10:46 -0400

    added being able to specify page number and skip pages in pagination

--- app/cases/directives/pagination.js
+++ app/cases/directives/pagination.js
@@ -2,27 +2,66 @@
 
 class Pagination {
     constructor($scope) {
-        $scope.pageSize = 15;
-        $scope.currentPage = 0;
+        // this is 4 because currentPage range is 0 ... n-1.
+        $scope.skipSize = 4;
+        $scope.onlyNumbers = /^\d+$/;
+        $scope.disablePageInput = false;
+        $scope.pageData = {
+            currentPage: 0,
+            currentPageNumber: 1,
+            pageSize: 15
+        };
 
-        $scope.numberOfPages = () => Math.ceil($scope.datalength / $scope.pageSize);
-        $scope.getCurrentPage = () => $scope.currentPage + 1;
+        // ng-model-options makes input only throw even when  user clicks outside of input (blur) or presses "enter" (change)
+        // http://embed.plnkr.co/2hCAxnClv68Dl5c06Brm/ https://docs.angularjs.org/api/ng/directive/ngModelOptions
+        $scope.ngModelOptions = {
+            updateOn: 'change blur'
+        };
+
+        $scope.numberOfPages = () => Math.ceil($scope.datalength / $scope.pageData.pageSize);
+        $scope.getCurrentPage = () => $scope.pageData.currentPage + 1;
         $scope.isForwardBtnDisabled = () => $scope.getCurrentPage() === $scope.numberOfPages();
         $scope.isBackBtnDisabled = () => $scope.getCurrentPage() === 1;
 
-        $scope.goForward = () => {
-            $scope.currentPage++;
-            $scope.setParentScopeVariables();
+        $scope.goForward = () => $scope.pageData.currentPage++;
+        $scope.skipForward = () => {
+            const numberOfPages = $scope.numberOfPages();
+            const isLessThanNumOfPages = $scope.getCurrentPage() + $scope.skipSize < numberOfPages;
+
+            if (isLessThanNumOfPages) {
+                $scope.pageData.currentPage += $scope.skipSize;
+            } else {
+                $scope.pageData.currentPage = numberOfPages - 1;
+            }
         };
 
-        $scope.goBack = () => {
-            $scope.currentPage--;
-            $scope.setParentScopeVariables();
+        $scope.goBack = () => $scope.pageData.currentPage--;
+        $scope.skipBack = () => {
+            const isGTEZero = $scope.getCurrentPage() - $scope.skipSize >= 0;
+
+            if (isGTEZero) {
+                $scope.pageData.currentPage -= $scope.skipSize;
+            } else {
+                $scope.pageData.currentPage = 0;
+            }
         };
+
+        $scope.onPageChange = () => console.log('hello');
 
         $scope.setParentScopeVariables = () => $scope.setdata({
-            pageSize: $scope.pageSize,
-            currentPage: $scope.currentPage
+            pageSize: $scope.pageData.pageSize,
+            currentPage: $scope.pageData.currentPage
+        });
+
+        $scope.$watch('pageData.currentPage', () => {
+            $scope.pageData.currentPageNumber = $scope.pageData.currentPage + 1;
+            $scope.setParentScopeVariables();
+        });
+
+        $scope.$watch('pageData.currentPageNumber', (newv) => {
+            if (newv && newv >= 1 && newv <= $scope.numberOfPages()) {
+                $scope.pageData.currentPage = newv - 1;
+            }
         });
 
         $scope.setParentScopeVariables();
