6c269eac50fbf3847b1eb4734adee02eeb9ad9eb
prani <prani@redhat.com>
2019-01-14 17:12:28 +0530

    simplifies linking bz

--- app/cases/services/linkifyService.js
+++ app/cases/services/linkifyService.js
@@ -5,25 +5,17 @@ export default class LinkifyService {
         this.linkifyWithCaseIDs = function (text) {
             if (RHAUtils.isEmpty(text)) return '';
 
-            var caseIdRegex = /([\s\('"]|^)(\d{8})([\s\)'"\.]|$)/g;
-            var caseLinkRegex = /<a href="#\/case\/\d{8}">\d{8}<\/a>/g;
-            var caseLinksinkifiedText = text.replace(caseIdRegex, '$1<a href="#/case/$2">$2</a>$3');
-            var textSegments = caseLinksinkifiedText.split(caseLinkRegex);
-            var caseLinks = caseLinksinkifiedText.match(caseLinkRegex);
-            // merge the elements back together and linkify textSegments
-            try {
-                var result = [$filter('linky')(textSegments[0], '_blank')];
-                for (var i = 1; i < textSegments.length; i++) {
-                    result.push(caseLinks[i - 1]);
-                    result.push($filter('linky')(textSegments[i], '_blank'));
-                }
-                return result.join('');
-            } catch(error) {
-                // https://projects.engineering.redhat.com/browse/PCM-6168
-                // Returning comment text as it is if there is any error while linkifying the content inside the comment text
-                return text;
-            }
+            var caseIdRegex = /([\s\('";]|^)(\d{8})([\s\)'"\&]|$)/g;
+            let caseLinksinkifiedText = text.replace(caseIdRegex, '$1<a href="#/case/$2">$2</a>$3');
+            return caseLinksinkifiedText;
+        }
 
+        this.linkifyBZIDs = function (text) {
+            if (RHAUtils.isEmpty(text)) return '';
+
+            var bzIdRegex = /(Bug|BZ)([\s\('";#]|^)(\d{7,})([\s\)'"\&]|$)/ig;
+            var bzLinkifiedText = text.replace(bzIdRegex, '<a href="https://bugzilla.redhat.com/show_bug.cgi?id=$3">$1$2$3</a>$4');
+            return bzLinkifiedText;
         }
     }
 }
