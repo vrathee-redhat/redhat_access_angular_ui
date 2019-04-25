37a7fcb46486dfe43e8ec4e5a9b83e41fafe27f1
prani <prani@redhat.com>
2019-04-25 14:02:53 +0530

    Revert "removes this.savePartnerCaseAccess"
    This reverts commit 1af3f680bc68a1594b46fdcc6566483cdbaa8190.

--- app/cases/controllers/shareCaseWithPartner.js
+++ app/cases/controllers/shareCaseWithPartner.js
@@ -2,7 +2,7 @@
 import { getTnCUrl } from '../../shared/TnC';
 import isEmpty from 'lodash/isEmpty';
 import get from 'lodash/get';
-import hydrajs from '../../shared/hydrajs';
+
 export default class ShareCaseWithPartner {
     constructor($scope, CaseService, AlertService, CASE_EVENTS, gettextCatalog) {
         'ngInject';
@@ -11,8 +11,6 @@ export default class ShareCaseWithPartner {
         $scope.selectedPartners = [];
 
         this.removeQueryParams = () => {
-            // Removing QP after successfully consuming these params to get clean slate,
-            // so now if user hits refresh button, it will not start "Sharing Case with partner" Patch request. 
             const url = window.location.href;
             const x = url.replace(/&?partnerAccountNumber=(\d+)/gi, '');
             const y = x.replace(/&?decision-\d+=\w+/gi, '');
@@ -26,19 +24,10 @@ export default class ShareCaseWithPartner {
             const ackID = get((/ackID=(\d+)/gi).exec(window.location.href.toString()), [1]);
             if(partnerAccountNumber && decisionAccepted && ackID) {
                 var alert = AlertService.addWarningMessage(gettextCatalog.getString(`Sharing Case with ${partnerAccountNumber}`));
-                CaseService.sharingCaseWithPartner = true;
-                const body = {
-                    access: { permission: 'Write', accountNumber: partnerAccountNumber },
-                    confirmationNumber: ackID
-                }
-                try {
-                    await hydrajs.kase.access.patchCaseAccess(CaseService.kase.case_number, body);
-                    this.removeQueryParams();
-                } catch (e) {
-                    AlertService.addDangerMessage(e.message);
-                }
+                await CaseService.savePartnerCaseAccess(CaseService.kase.case_number, partnerAccountNumber, ackID);
                 CaseService.sharingCaseWithPartner = false;
                 AlertService.removeAlert(alert);
+                this.removeQueryParams();
             }
             await CaseService.populatePartners(CaseService.kase.case_number, CaseService.kase.account_number);
         };
